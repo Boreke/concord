680e19b3744253ba2a72416fee0df82c9fd549c8
Boreke <hippomartin03@gmail.com>
2025-06-04 18:48:50 +0200

    resolving reviews

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 // https://astro.build/config
 export default defineConfig({
